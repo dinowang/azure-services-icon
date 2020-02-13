094f558e43e7201014e32963062a14b31885e6e3
Dino Wang <dino.cool@gmail.com>
2020-02-13 12:05:14 +0800

    update

--- ServiceMapCrawler/Program.cs
+++ ServiceMapCrawler/Program.cs
@@ -47,6 +47,60 @@ namespace ServiceMapCrawler
                     inuptStream.CopyTo(outputStream);
                 }
             });
+
+            var regions = model
+                            .Where(x => x.Value.Availability != null)
+                            .SelectMany(x => x.Value.Availability?.Select(y => y.Key))
+                            .ToHashSet();
+
+            var serviceSheet = Path.Combine(basePath, "service-sheet.html");
+
+            using (var fileStream = new FileStream(serviceSheet, FileMode.Create, FileAccess.ReadWrite))
+            using (var writer = new StreamWriter(fileStream))
+            {
+                writer.WriteLine("<html><head><style>table { border-collapse: collapse; } th, td { border: solid 1px black; padding: 2px 3px; word-break: keep-all; }</style></head><body>");
+                writer.WriteLine("<table>");
+                writer.WriteLine("<thead>");
+                writer.WriteLine("<tr>");
+                writer.WriteLine("<th></th>");
+                foreach (var regionName in regions)
+                {
+                    writer.WriteLine($"<th>{regionName}</th>");
+                }
+                writer.WriteLine("</tr>");
+                writer.WriteLine("</thead>");
+
+                writer.WriteLine("<tbody>");
+                foreach (var serviceName in model.Keys)
+                {
+                    writer.WriteLine("<tr>");
+                    writer.WriteLine($"<th>{serviceName}</th>");
+                    foreach (var regionName in regions)
+                    {
+                        writer.WriteLine("<td>");
+                        var service = model[serviceName];
+                        if (service.Availability != null && service.Availability.ContainsKey(regionName))
+                        {
+                            var availability = service.Availability[regionName];
+
+                            if (!availability.Available)
+                                writer.WriteLine("&nbsp;");
+                            else if (availability.InGA)
+                                writer.WriteLine("✔️");
+                            else if (availability.InPreview)
+                                writer.WriteLine("🔘");
+
+                            if (!string.IsNullOrEmpty(availability.Expectation))
+                                writer.WriteLine("<br>" + availability.Expectation);
+                        }
+                        writer.WriteLine("</td>");
+                    }
+                    writer.WriteLine("</tr>");
+                }
+                writer.WriteLine("</tbody>");
+                writer.WriteLine("</table>");
+                writer.WriteLine("</body></html>");
+            }
         }
     }
 
