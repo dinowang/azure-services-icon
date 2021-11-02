bfd72a8aaa2196d8952f07ebc81f5936bc442c7f
Dino Wang <dino.cool@gmail.com>
2021-11-03 01:56:01 +0800

    20211103 update

---
+++ Others/update.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+wget https://go.microsoft.com/fwlink/?linkid=869455 -O m365.zip
+
+wget https://download.microsoft.com/download/3/e/a/3eaa9444-906f-468d-92cb-ada53e87b977/Dynamics_365_Icons_scalable.zip -O d365.zip
+
+wget https://download.microsoft.com/download/e/f/4/ef434e60-8cdc-4dd1-9d9f-e58670e57ec1/Power_Platform_scalable.zip -O powerplatform.zip
+
+wget https://github.com/gilbarbara/logos/archive/refs/heads/master.zip -O gilbarbara.zip
